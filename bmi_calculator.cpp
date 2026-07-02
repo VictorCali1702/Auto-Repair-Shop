c563e1b87b5f149a4cb38b4854fe1648deb7c077
VictorCali1702 <wiktor.naczk199@gmail.com>
2026-07-02 21:40:17 +0200

    C++ calculator bmi

--- bmi_calculator.cpp
+++ bmi_calculator.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main () {
     string imie;
-    float waga;
+    double waga;
     string plec;
     int wiek;
+    double wzrost;
 
     cout << "=== Witaj ===\n";
     cout << "=== Kalkulator BMI ===\n";
 
-    cout << "Podaj swoje imię:";
+    cout << "Podaj swoje imię: ";
     cin >> imie;
+    cout << "Podaj płeć: ";
+    cin >> plec;
+    cout << "Podaj wiek: ";
+    cin >> wiek;
+    cout << "Podaj wagę (kg): ";
+    cin >> waga;
+    cout << "Podaj wzrost (cm): ";
+    cin >> wzrost;
 
+    double wzrost_m = wzrost / 100;
+
+    double BMI = waga / pow(wzrost_m, 2);
+
+    cout << "BMI wynosi: " << BMI;
+    cout << "\nTwoje BMI: ";
     
+    if (BMI <= 18.5){
+        cout << "Niedowaga";
+    } else if (BMI <= 24.9){
+        cout << "Prawidłowa masa ciała";
+    } else if (BMI <= 29.9) {
+        cout << "Nadwaga";
+    } else {
+        cout << "Otyłość";
+    }
+
+
+
 
     return 0;
 }
