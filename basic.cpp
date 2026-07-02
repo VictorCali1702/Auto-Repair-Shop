0b4be2701db1ca7221416d6644f1df271033d2a0
VictorCali1702 <wiktor.naczk199@gmail.com>
2026-07-02 21:23:02 +0200

    C++ calculator bmi

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
     string name;
-     
+    int age;
+    cout << "Hello, what is your name? ";
+    cin >> name;
+    cout << "How old are you? ";
+    cin >> age;
+    
+    cout << "Hello " << name << ". Nice to meet you!\n";
+    cout << "You are " << age << " years old!\n";
+
+    if (age >= 18) {
+        cout << "You are adult.";
+    } else {
+        cout << "You are a child.";
+    }
+
     return 0;
 }
---
+++ bmi_calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    string imie;
+    float waga;
+    string plec;
+    int wiek;
+
+    cout << "=== Witaj ===\n";
+    cout << "=== Kalkulator BMI ===\n";
+
+    cout << "Podaj swoje imię:";
+    cin >> imie;
+
+    
+
+    return 0;
+}
