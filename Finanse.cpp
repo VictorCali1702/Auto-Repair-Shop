274d7e597aae030a4957a362cfe388bdb78ee9e9
VictorCali1702 <wiktor.naczk199@gmail.com>
2026-07-12 19:41:42 +0200

    Building a CLI Finanse program

---
+++ Finanse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    cout << "Witaj w programie Finanse osobiste!";
+
+    return 0;
+}
