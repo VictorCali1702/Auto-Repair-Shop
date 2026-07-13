4e386811a3beb02790e77fd37bfe3629b98dd476
VictorCali1702 <wiktor.naczk199@gmail.com>
2026-07-13 21:37:26 +0200

    Building a CLI Finanse program

--- Finanse.cpp
+++ Finanse.cpp
@@ -6,7 +6,19 @@
 using namespace std;
 
 int main() {
-    cout << "Witaj w programie Finanse osobiste!";
+    cout << "Witaj w programie Finanse osobiste!\n";
+    double przychod;
 
+    cout << "Podaj przychód brutto: ";
+    cin >> przychod;
+    cout << "\nPodany przychód brutto: " << przychod << " PLN\n";
+    
+    double podatek = 32;
+    double podatekKwota = przychod * (podatek / 100.0);
+    cout << "\nPodatek od przychodu " << przychod << " PLN wynosi: " << podatekKwota << endl;
+    
+    double kwotaNetto = przychod - podatekKwota;
+    cout << "\nPrzychód Netto: " << kwotaNetto << " PLN";
+    
     return 0;
 }
